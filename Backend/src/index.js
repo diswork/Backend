e00dac34e114ca714c0d8af723358185cb87fd2d
hugosmhd <47651460+hugosmhd@users.noreply.github.com>
2019-07-22 18:29:13 -0600

    cambiando db a mlab

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const app = require("./app");
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb+srv://hugos:kinal2017252@@dbdiswork-xbykg.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true }).then(()=>{
+mongoose.connect('mongodb://hugos:kinal2017252@ds253567.mlab.com:53567/heroku_6j5hjpjw', { useNewUrlParser: true }).then(()=>{
     console.log('Se encuentra conectado a la Base de Datos');
 
     app.set('port', process.env.PORT || 3000 );
